f01c8a7fb73723469b57344610dc8b8f1edaa19a
emma <enabojohn@gmail.com>
2020-12-03 23:14:33 +0300

    Innitial commit Changed static elements from staic html to a dynamic react app

---
+++ src/data.js
@@ -0,0 +1,77 @@
+const data =   {
+    products: [
+        {
+            _id: '1',
+            name:'Slim Shirt',
+            catergory:'Shirts',
+            image:'/images/d1.jpg',
+            price:60,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6 
+        },
+        {
+         _id:'2',
+         name:'Fit Shirt',
+         catergory:'Shirts',
+         image:'/images/d2.jpg',
+         price:50,
+         brand:'Nike', 
+         rating:4.7,
+         numReviews:10,
+         countInStock: 5
+           
+       
+     },
+     {   
+         _id:'3',
+         name:'Fitting Shirt',
+         catergory:'Shirts',
+         image:'/images/d3.jpg',
+         price:70,
+         brand:'Nike',
+         rating:4.3,
+         numReviews:8,
+         countInStock: 6
+     
+
+     },
+     {
+         _id:'4',
+         name:'Cool Pant',
+         catergory:'Pants',
+         image:'/images/P4.jpg',
+         price:50,
+         brand:'Nike', 
+         rating:4.6,
+         numReviews:10,
+         countInStock: 7
+         
+     },
+     {
+         _id:'5',
+         name:'Nice Pant',
+         catergory:'Pants',
+         image:'/images/P5.jpg',
+         price:50,
+         brand:'Nike', 
+         rating:4.0,
+         numReviews:10, 
+         countInStock: 8  
+     },
+     {
+         _id:'6',
+         name:'Fit pant',
+         catergory:'Pants',
+         image:'/images/P6.jpg',
+         price:50,
+         brand:'Nike', 
+         rating:4.2,
+         numReviews:10,
+         countInStock: 4
+
+     }
+    ]
+ }
+ export default data
