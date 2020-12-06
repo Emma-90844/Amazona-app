e21612a0c31610a8fea6b1576f272bfbebf31642
emma <enabojohn@gmail.com>
2020-12-06 11:40:18 +0300

    Implemented Details product action

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,8 +1,8 @@
 
 import Axios from 'axios';
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from './../constants/productConstants';
+import { PRODUCT_DETAILS_REQUEST, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from './../constants/productConstants';
 
-
+// List Products action
 export const listProducts = () => async (dispatch) => {
     dispatch({
         type: PRODUCT_LIST_REQUEST
@@ -10,10 +10,28 @@ export const listProducts = () => async (dispatch) => {
     try {
         const { data } = await Axios.get('/api/products');
         dispatch({
-            type: PRODUCT_LIST_SUCCESS, payload: data })
+            type: PRODUCT_LIST_SUCCESS, payload: data
+        })
     } catch (error) {
         dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message })
     }
+}
+
+// Product details action
+export const detailsProduct = (productId) => async (dispatch) => {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
+    try {
+        const { data } = await Axios.get(`/api/products/${productId}`);
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS, payload: data
+        });
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL, payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        });
+    };
 }
 
 
