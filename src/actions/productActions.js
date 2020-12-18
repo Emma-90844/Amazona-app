9b2d810f26a72656aa4daa3b32407511bd153e7e
emma <enabojohn@gmail.com>
2020-12-18 08:56:06 +0300

    Created a select box that only show if count in stock is greater than zero and if content is less than zero the add to cart button won't show

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,17 +1,21 @@
 
 import Axios from 'axios';
-import { PRODUCT_DETAILS_REQUEST, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from './../constants/productConstants';
+import {
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
+} from '../constants/productConstants';
 
 // List Products action
 export const listProducts = () => async (dispatch) => {
-    dispatch({
-        type: PRODUCT_LIST_REQUEST
-    });
     try {
-        const { data } = await Axios.get('/api/products');
-        dispatch({
-            type: PRODUCT_LIST_SUCCESS, payload: data
-        })
+        dispatch({ type: PRODUCT_LIST_REQUEST });
+        const { data } = await Axios.get("/api/products");
+        console.log(data)
+        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
     } catch (error) {
         dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message })
     }
@@ -22,14 +26,13 @@ export const detailsProduct = (productId) => async (dispatch) => {
     dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId });
     try {
         const { data } = await Axios.get(`/api/products/${productId}`);
-        dispatch({
-            type: PRODUCT_DETAILS_SUCCESS, payload: data
-        });
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+
     } catch (error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL, payload: error.response && error.response.data.message
                 ? error.response.data.message
-                : error.message
+                : error.message,
         });
     };
 }
--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,18 +1,38 @@
 
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from './../constants/productConstants';
+import {
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
+} from './../constants/productConstants';
 
 
 
-// Set products to an empty arrays otherwise the results will be null
-export const productListReducer = (state = {loading: true, products:[]}, action) => {
-    switch(action.type){
+// NB: Set products to an empty arrays otherwise the results will be null
+export const productListReducer = (state = { loading: true, products: [] }, action) => {
+    switch (action.type) {
         case PRODUCT_LIST_REQUEST:
             return { loading: true };
         case PRODUCT_LIST_SUCCESS:
-            return  { loading: false, products: action.payload };
+            return { loading: false, products: action.payload };
         case PRODUCT_LIST_FAIL:
-             return { loading: false, error: action.payload };
-            default:
-                return state;
+            return { loading: false, error: action.payload };
+        default: 
+            return state;
+    }
+}
+// Product details reducer
+export const productDetailsReducer = (state = { product: {}, loading: true }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload };
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
     }
 }
