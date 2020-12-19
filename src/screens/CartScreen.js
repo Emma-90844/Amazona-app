8ceaeb6684513473e420749ee619adbb54807255
emma <enabojohn@gmail.com>
2020-12-19 10:36:08 +0300

    Implemented add to cart action and saved the cart in local storage, Items in the cart stays even when user close the browser and open again.

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { addToCart } from '../actions/cartActions';
+import { useDispatch } from 'react-redux';
 
 export default function CartScreen(props) {
     const productId = props.match.params.id;
     // props.location.search: this value returns cart/qty={qty} in the productscree redirect history
-    const qty  = props.location.search ? Number(props.location.search.split('=')[1]): 1;
+    const qty  = props.location.search
+     ? Number(props.location.search.split('=')[1]): 1;
+    
+    const dispatch = useDispatch();
+     useEffect(() => {
+      if(productId){
+        dispatch(addToCart(productId, qty))
+      }
+     },[dispatch, productId, qty])
+
+   
   return (  
     <div>
         <h1>Cart Screen</h1>
