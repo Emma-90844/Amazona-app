0d519ca03ca93ddf7b4f89b1b0136000f0fe0b8f
emma <enabojohn@gmail.com>
2020-12-19 10:30:49 +0300

    Added a new reducer for the cart in the store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,21 @@
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux'; 
 import thunk from 'redux-thunk'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
-const initialState = {};
+
+// Innitial value for the cart
+const initialState = {
+    cart:{
+        cartItems: localStorage.getItem('cartItems')
+        ? JSON.parse( localStorage.getItem('cartItems')):[],
+        
+    }
+};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart:cartReducer
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
