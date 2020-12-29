7d3db24ae75e1b0ac13f4e40d16041eef0081081
emma <enabojohn@gmail.com>
2020-12-29 16:30:33 +0300

    Defined a new action to remove items from the cart

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,41 +1,34 @@
 import Axios from "axios";
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import Cookie from 'js-cookie'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
+    //  Add to Cart action  
     const { data } = await Axios.get(`/api/products/${productId}`);
     dispatch({
-        type: CART_ADD_ITEM, payload: {
+        type: CART_ADD_ITEM,
+        payload: {
             name: data.name,
-            image: data.image, 
+            image: data.image,
             price: data.price,
             countInStock: data.countInStock,
             product: data._id,
-            qty, 
+            qty,
         }
     });
-     //  Saving the added cart item to the local storage
-     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+    //  Saving the added cart item to the local storage
+    localStorage.setItem(
+        'cartItems',
+        JSON.stringify(getState().cart.cartItems));
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Remove item from Cart
+export const removeFromCart = (productId) => (dispatch, getState) => {
+    dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+}
 
 
 
