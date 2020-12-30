c07fb08a9fc48b35e028d33a22650f0f5d9bcce4
emma <enabojohn@gmail.com>
2020-12-30 10:30:19 +0300

    Implemented show logged in user in the header

--- src/App.js
+++ src/App.js
@@ -3,43 +3,70 @@ import { BrowserRouter, Route } from "react-router-dom"
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
-import {Link} from 'react-router-dom'
-import { useSelector } from 'react-redux';
-
-
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
+import logo from './components/logo.png'
+import SigninScreen from './screens/SigninScreen';
+import { signout } from './actions/userActions';
 
 function App() {
+
+
   const cart = useSelector(state => state.cart);
-  const { cartItems} = cart;
+  const { cartItems } = cart;
+  // to get user sigin
+  const userSignin = useSelector((state) => state.userSignin)
+  const { userInfo } = userSignin;
+
+// 
+const dispatch = useDispatch();
+const signoutHandler = () => {
+  dispatch(signout())
+}
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
+          {/* LOGO */}
           <div>
             <Link className="brand" to="/">
-               amazona
-          </Link>
-          </div>
-          <div>
-            <Link to="/cart">Cart 
-               {cartItems.length > 0 && (
-              <span className="badge">{cartItems.length}</span>
-            )}
+
+              <img src={logo} alt="logo" className="logo"></img>
             </Link>
-            <Link to="/signin">Sign In</Link>
           </div>
+
+          {/* LOGO */}
+          <div>
+            <Link to="/cart">Cart
+               {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {/* Conditional rendering of user signin name in place of sign in button */}
+            {
+              userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">{userInfo.name}<i className="fa fa-caret-down"></i></Link>
+                  <ul className="dropdown-content">
+                    <Link to="#signin" onClick={signoutHandler}>Sign Out</Link>
+                  </ul>
+                </div>
+              ) : (<Link to="/signin">Sign In</Link>)
+            }
+
+          </div>
+
 
         </header>
         <main>
           <Route path="/cart/:id?" component={CartScreen} ></Route>
           <Route path="/product/:id" component={ProductScreen} exact></Route>
+          <Route path="/signin" component={SigninScreen} ></Route>
           <Route path="/" component={HomeScreen} exact></Route>
-
         </main>
         <footer className="row center">
-           All rights reserved, Incoparated
+          All rights reserved, Incoparated
     </footer>
-
       </div>
     </BrowserRouter>
   );
