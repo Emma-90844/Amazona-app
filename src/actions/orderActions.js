f1ef7d0f82e8ac34ab1bf1c31a3e92c79a131b96
emma <enabojohn@gmail.com>
2021-01-08 11:46:51 +0300

    Created a new order to get order details from the backend

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_FAIL, ORDER_CREATE_SUCCESS } from './../constants/orderConstants';
+import { ORDER_CREATE_REQUEST, ORDER_CREATE_FAIL, ORDER_CREATE_SUCCESS, ORDER_DETAILS_REQUEST, ORDER_DETAILS_FAIL, ORDER_DETAILS_SUCCESS } from './../constants/orderConstants';
 import { CART_EMPTY } from './../constants/cartConstants';
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -29,6 +29,52 @@ export const createOrder = (order) => async (dispatch, getState) => {
         });
     }
 }
+
+
+
+
+// Details order
+export const detailsOrder = (orderId) => async(dispatch, getState) => {
+    dispatch({ type: ORDER_DETAILS_REQUEST, payload: orderId });
+    try{
+        const { userSignin: { userInfo } } = getState()
+        const { data } = await Axios.get(`/api/orders/${orderId}`, {
+            headers: {Authorization: `Bearer ${userInfo.token}`},
+        });
+        dispatch({ type: ORDER_DETAILS_SUCCESS, payload: data});
+    }catch(error){
+        const message = error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+        dispatch({ type: ORDER_DETAILS_FAIL, payload: message})
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
