94ffba5b2f5cfe85780bba988976aa56199f048e
emma <enabojohn@gmail.com>
2021-01-13 13:20:08 +0300

    Added a new reducer for user details.

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,5 +1,5 @@
 
-import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_SIGNOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL } from './../constants/userConstants';
+import { USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAIL, USER_SIGNIN_SIGNOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL } from './../constants/userConstants';
 
 
 export const userSigninReducer = (state = {}, action) => {
@@ -30,7 +30,20 @@ export const userRegisterReducer = (state = {}, action) => {
                 return state
     }
 }
-
+// User profile
+export const userDetailsReducer = (state = {loading: true}, action) => {
+    switch(action.type){
+        case USER_DETAILS_REQUEST:
+            return {loading: true}
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload };
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+       
+            default:
+                return state
+    }
+}
 
 
 
